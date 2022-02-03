3b2ab60416859ad27f9c8e93d5678e54e70b85bd
razrodrigues <rafael.rodrigues@klab.com.br>
2022-02-03 17:28:06 -0300

    Finalizado implementação de geração de token JWT

--- src/main/java/com/estudos/encriptarsenhausuario/EncriptarSenhaUsuarioApplication.java
+++ src/main/java/com/estudos/encriptarsenhausuario/EncriptarSenhaUsuarioApplication.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class EncriptarSenhaUsuarioApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/estudos/encriptarsenhausuario/model/UsuarioModel.java
+++ src/main/java/com/estudos/encriptarsenhausuario/model/UsuarioModel.java
@@ -1,5 +1,6 @@
 package com.estudos.encriptarsenhausuario.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,6 @@ public class UsuarioModel {
     private Integer id;
     @Column(unique = true)
     private String login;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
